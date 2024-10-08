35459fbc4103ae638a56ae121cfe3eac703a3ec8
ishika vishwakarma <132936647+ishikavishwakarma@users.noreply.github.com>
2024-10-08 13:22:51 +0530

    second

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views//*.{ejs}"],
+  content: [
+    "./views/**/*.ejs", // Keep your original content path
+    "./node_modules/flowbite/**/*.js" // Add flowbite content path
+  ],
   theme: {
-  extend: {},
+    extend: {},
   },
   plugins: [
-  {
-  tailwindcss: {},
-  autoprefixer: {},
-  },
-  ],
+    require('flowbite/plugin'), // Add flowbite plugin
+  ],
   };
